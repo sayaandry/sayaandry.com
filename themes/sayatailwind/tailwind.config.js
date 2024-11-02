4549aefccac34bfc00944c4e730909f314ca2bcf
Andry Setiawan <sayaandryxyz@gmail.com>
2024-11-02 21:02:46 +0700

    install tailwindcss

---
+++ themes/sayatailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'layouts/**/*.html',
+    'content/**/*.{html,md}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
+
